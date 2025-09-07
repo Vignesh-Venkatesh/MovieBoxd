d11dbb730f5dd951c7676c55d439d412f94eef8c
Vignesh Venkatesh <vigneshvenkatesh2k3@gmail.com>
2025-09-06 22:18:56 -0400

    components/cast/* - comments

--- client/src/components/cast/CastList.tsx
+++ client/src/components/cast/CastList.tsx
@@ -7,23 +7,27 @@ type CastListProps = {
 };
 
 export default function CastList({ cast }: CastListProps) {
-  // if cast is [], then we dont render anything
+  // If there is no cast or the array is empty, render nothing
   if (!cast || cast.length === 0) {
     return null;
   }
 
-  // if cast exists, then we render
+  // If cast exists, render the cast list
   return (
     <div>
+      {/* Section title */}
       <Title title="Cast" />
 
+      {/* Cast members grid */}
       <div className="flex flex-wrap gap-1 mt-2">
         {cast.map((member, idx) => (
           <Link to={`/person/${member.id}`} key={idx}>
             <div className="bg-neutral text-xs rounded px-2 py-1 hover:bg-base-200 tooltip tooltip-top">
+              {/* Character name shown on hover */}
               <h1 className="bg-green-600 text-black rounded font-semibold tooltip-content text-xs">
                 {member.character}
               </h1>
+              {/* Actor name displayed */}
               <h1>{member.name}</h1>
             </div>
           </Link>
